ddee58eb658e88fcc87adfde75928cb5c19c34f4
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-08-01 03:44:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
 	 let data = document.getElementsByTagName('input')[0].value;
-    let data1 = document.getElementsByTagName('input')[1].value;
-    alert(data+data1);
+     let data1 = document.getElementsByTagName('input')[1].value;
+    alert(data+" "+data1);
 }
